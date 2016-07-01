24048fd0974a7313a5d1641321de8e42bb6b8327
Marcus Ball <marcus@marcusball.me>
2016-06-30 21:08:18 -0400

    hooray it works!

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,89 @@ use std::time::Duration;
 
 
 
-struct MyHandler;
+struct MinerBotHandler{
+    host: String,
+    port: u16
+}
+
+impl MinerBotHandler{
+    fn new(host: String, port: u16) -> MinerBotHandler{
+        MinerBotHandler{
+            host: host,
+            port: port,
+        }
+    }
+
+    fn get_stream(&self) -> TcpStream{
+        let stream = TcpStream::connect((self.host.as_str(), self.port)).unwrap();
+        let _ = stream.set_read_timeout(Some(Duration::from_secs(3)));
+        let _ = stream.set_write_timeout(Some(Duration::from_secs(1)));
+        return stream;
+    }
+
+
+    fn get_server_status(&mut self) -> String{
+        let server_data = minecraft_server_info::query_server(&mut self.get_stream(), &self.host, self.port);
+        if let Ok(data) = server_data{
+            return format!("Server is up; description: \"{}\"", data.description.text);
+        }
+        else{
+            return "Failed to receive server info, maybe it's down?".to_string();
+        }
+    }
+
+    fn get_online_message(&mut self) -> String{
+        let server_data = minecraft_server_info::query_server(&mut self.get_stream(), &self.host, self.port);
+        if let Ok(data) = server_data{
+            if data.players.online > 0{
+                let len = data.players.online;
+                let players = data.players.sample.unwrap().iter()
+                    .map(|player_data| &player_data.name)
+                    .fold(("".to_string(), 0), |(mut phrase, mut index), word|{
+                        if index > 0{
+                            if len != 2{ phrase.push_str(","); }
+                            if index == len - 1 { phrase.push_str(" and "); }
+                            else{ phrase.push(' '); }
+                        }
+                        phrase.push_str(word.to_string().as_str());
+                        index += 1;
+                        (phrase, index)
+                    }).0;
+                return format!("{} {} online",players, match data.players.online { 1 => "is", _ => "are"});
+            }
+            else{
+                return "No one is online".to_string();
+            }
+        }
+        else{
+            return "Failed to receive server info, maybe it's down?".to_string();
+        }
+    }
+}
 
 #[allow(unused_variables)]
-impl slack::EventHandler for MyHandler {
+impl slack::EventHandler for MinerBotHandler {
     fn on_event(&mut self, cli: &mut slack::RtmClient, event: Result<&slack::Event, slack::Error>, raw_json: &str) {
         println!("on_event(event: {:?}, raw_json: {:?})", event, raw_json);
 
         if let Ok(&slack::Event::Message(ref message)) = event{
             match message{
-                &slack::Message::Standard{ ref text, .. } => {
+                &slack::Message::Standard{ ref text, ref channel, .. } => {
                     let mes = text.clone().unwrap();
-                    println!("Received message \"{}\"", mes);
+                    let channel = channel.clone().unwrap();
 
-                    if mes.as_str().starts_with("!test"){
+                    if mes.as_str().starts_with("!server"){
                         println!("Received server command");
+                        let status_message = self.get_server_status();
+                        let _ = cli.send_message(channel.as_str(), status_message.as_str());
+                    }
+                    else if mes.as_str().starts_with("!online"){
+                        println!("Received online status query.");
+                        let online_message =  self.get_online_message();
+                        let _ = cli.send_message(channel.as_str(), online_message.as_str());
+                    }
+                    else{
+                        println!("Received message \"{}\"", mes);
                     }
                 },
                 _ => { println!("Received some other message"); }
@@ -53,25 +121,6 @@ fn main() {
     let host = "silvermoon.online";
     let port = 25565;
 
-    // let mut stream = TcpStream::connect((host,port)).unwrap();
-    // let _ = stream.set_read_timeout(Some(Duration::from_secs(3)));
-    // let _ = stream.set_write_timeout(Some(Duration::from_secs(1)));
-    //
-    // let data = minecraft_server_info::query_server(&mut stream, &String::from(host), port).unwrap();
-    // println!("{:?}", &data);
-    //
-    // println!("{}", data.description.text);
-    // println!("");
-    // if data.players.online > 0{
-    //     println!("There are {} players online: ", data.players.online);
-    //     for player in data.players.sample.unwrap(){
-    //         println!("{}", player.name);
-    //     }
-    // }
-    // else{
-    //     println!("No one is online!");
-    // }
-
     let args: Vec<String> = std::env::args().collect();
     let api_key = match args.len() {
         0 | 1 => panic!("No api-key in args! Usage: cargo run -- <api-key>"),
@@ -79,14 +128,66 @@ fn main() {
             args[x - 1].clone()
         }
     };
-    
-    let mut handler = MyHandler;
+
+    let mut handler = MinerBotHandler::new(host.to_string(), port);
     let mut cli = slack::RtmClient::new(&api_key);
-    let r = cli.login_and_run::<MyHandler>(&mut handler);
+    let r = cli.login_and_run::<MinerBotHandler>(&mut handler);
     match r {
         Ok(_) => {}
         Err(err) => panic!("Error: {}", err),
     }
     println!("{}", cli.get_name().unwrap());
     println!("{}", cli.get_team().unwrap().name);
+}
+
+#[test]
+fn show_info(){
+    let host = "silvermoon.online";
+    let port = 25565;
+
+    let mut stream = TcpStream::connect((host,port)).unwrap();
+    let _ = stream.set_read_timeout(Some(Duration::from_secs(3)));
+    let _ = stream.set_write_timeout(Some(Duration::from_secs(1)));
+
+    let data = minecraft_server_info::query_server(&mut stream, &String::from(host), port).unwrap();
+    println!("{:?}", &data);
+}
+
+#[test]
+fn show_status(){
+    let host = "silvermoon.online";
+    let port = 25565;
+
+    let mut handler = MinerBotHandler::new(host.into(), port);
+
+    println!("{}", handler.get_server_status());
+}
+
+#[test]
+fn show_online(){
+    let host = "silvermoon.online";
+    let port = 25565;
+
+    let mut handler = MinerBotHandler::new(host.into(), port);
+
+    println!("{}", handler.get_online_message());
+}
+
+#[test]
+fn test_fold(){
+    let items = vec!["rick", "morty", "beth", "jerry", "summer"];
+
+    let len = items.len();
+    let string = items.iter().fold(("".to_string(), 0), |(mut phrase, mut index), word|{
+        if index > 0{
+            if len != 2{ phrase.push_str(","); }
+            if index == len - 1 { phrase.push_str(" and "); }
+            else{ phrase.push(' '); }
+        }
+        phrase.push_str(word.to_string().as_str());
+        index += 1;
+        (phrase, index)
+    }).0;
+
+    println!("{}", string);
 }
